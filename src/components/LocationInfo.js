e97ddee1bfbb54af4f6b34f77312b9ae85979473
juancastano0398 <juancastano0398@gmail.com>
2022-03-16 18:44:24 +0100

    Solucion de errores de asincronia

--- src/components/LocationInfo.js
+++ src/components/LocationInfo.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-const LocationInfo = ( {location} ) => {
-
-     console.log(location)
+const LocationInfo = ( {location} ) => {     
 
     return (
         <div className='content-location'>
-            <h1 className='title-locationinfo'> {location.name} </h1>
+            <h1 className='title-locationinfo'> {location?.name} </h1>
             <div className='card-location'>
-                <p><b>Type: </b>{location.type}</p>
-                <p><b>Dimension: </b>{location.dimension}</p>
-                <p><b>Number Residents: </b>{location.residents.length}</p>
+                <p><b>Type: </b>{location?.type}</p>
+                <p><b>Dimension: </b>{location?.dimension}</p>
+                <p><b>Number Residents: </b>{location.residents?.length}</p>
             </div>
         </div>
     );
